148b73ab16213491c2c7d2dbd4da400d32365ab5
Agata Bogacz <agatabogacz@outlook.com>
2023-01-19 16:51:54 +0900

    Change: production url from ip address to domain name.

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,4 +1,4 @@
-const PRODUCTION_URL = 'http://159.223.59.255'
+const PRODUCTION_URL = 'http://suzuchiginko.jp'
 const DEV_URL = 'http://localhost:3000'
 
 export const ALLOWED_CLIENT_URL = PRODUCTION_URL
